6cf6c63e38b40ee55520745e903aa6f8c6a23779
Omegalupi <80219233+Omegalupi@users.noreply.github.com>
2021-11-05 23:23:32 +0100

    Create ej6.c

---
+++ practica2.1/ej6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  printf("Longitud Máxima de Argumentos: %li\n", sysconf(_SC_ARG_MAX));
+  printf("Número máximo de hijos: %li\n", sysconf(_SC_CHILD_MAX));
+  printf("Número máximo de ficheros abiertos: %li\n", sysconf(_SC_OPEN_MAX));
+
+  return 1;
+}
