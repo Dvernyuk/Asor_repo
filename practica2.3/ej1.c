fb6d88c637edf13c478b7a1dac44705986885448
Omegalupi <80219233+Omegalupi@users.noreply.github.com>
2021-11-22 10:25:56 +0100

    Update ej1.c

--- practica2.3/ej1.c
+++ practica2.3/ej1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sched.h>
 
 int main() {
   int incr = -20;
@@ -10,7 +11,7 @@ int main() {
 
   incr = 12;
 
-  if(nice(incr) == -1)
+  if(sched_setscheduler(0, SCHED_FIFO, incr) == -1)
     printf("Error %d: %s\n", errno, strerror(errno));
 
   return 0;
