5738c4ffb9ed5020cd5d70a1b2ec6159dc972036
Omegalupi <80219233+Omegalupi@users.noreply.github.com>
2021-11-26 00:32:29 +0100

    Create ej2.c

---
+++ practica2.3/ej2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+int main() {
+	int pid = 0;
+	int sched = sched_getscheduler(pid);
+	int max = sched_get_priority_max(sched);
+	int min = sched_get_priority_min(sched);
+	struct sched_param sp;
+
+	printf("Planificación ");
+	switch (sched) {
+		case SCHED_OTHER:
+			printf("Other\n");
+		break;
+		case SCHED_FIFO:
+			printf("FIFO\n");
+		break;
+		case SCHED_RR:
+			printf("Round-Robin\n");
+		break;
+		default:
+			printf("Error\n");
+	}
+
+	sched_getparam(pid, &sp);
+	printf("Prioridad %i\n", sp.sched_priority);
+	printf("Prioridad max %i\n", max);
+	printf("Prioridad min %i\n", min);
+
+	return 0;
+}
