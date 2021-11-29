3c94754fff559f8a3dc851b74227f005029ce697
Omegalupi <80219233+Omegalupi@users.noreply.github.com>
2021-11-29 10:40:33 +0100

    Update ej1.c

--- practica2.4/ej1.c
+++ practica2.4/ej1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
+int main(int argc, char **argv) {
+	int ret = 0;
+	int fd[2];
+	pipe(fd);
+	pid_t pid = fork();
+
+	switch(pid) {
+		case -1:
+			printf("Error %d: %s\n", errno, strerror(errno));
+			ret = -1;
+		break;
+		case 0:
+			close(fd[0]);
+			dup2(fd[1], 1);
+			close(fd[1]);
+			execlp(argv[1], argv[1], argv[2], 0);
+		break;
+		default:
+			close(fd[1]);
+			dup2(fd[0], 0);
+			close(fd[0]);
+			execlp(argv[3], argv[3], argv[4], 0);
+	}
+
+	return ret;
+}
