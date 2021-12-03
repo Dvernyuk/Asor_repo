bb3b713e944b40db25931a8cbe92451ebd3c6d50
Omegalupi <80219233+Omegalupi@users.noreply.github.com>
2021-12-03 19:41:22 +0100

    Create ej4.c

---
+++ practica2.4/ej4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv){
+	int ret = 0;
+	int fd;
+	
+	if(mkfifo("tuberia", 0777) == -1){
+		printf("Error %d: %s\n", errno, strerror(errno));
+		ret = -1;
+	} else {
+		fd = open("tuberia", O_WRONLY, O_CREAT);
+		write(fd, argv[1], strlen(argv[1]));
+		close(fd);
+	}
+
+	return ret;
+}
